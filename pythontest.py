651d8838c09a6506a6e92f9a77eeb189d2bbbd9d
Billy Ng <bbng610@gmail.com>
2023-03-20 10:58:32 +0900

    added name class to see

--- pythontest.py
+++ pythontest.py
@@ -3,6 +3,7 @@ import django
 #import os
 #import sys
 import datetime
+import cgi
 
 def get_current_time():
     now = datetime.datetime.now()
@@ -10,9 +11,19 @@ def get_current_time():
 
 
 print(django.__version__)
-
-name ='Billy';
-
-print("Hello ", name);
-
 print (get_current_time())
+
+#!/usr/bin/env python3
+
+form = cgi.FieldStorage()
+name = form.getvalue("name")
+
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Greeting</title>")
+print("</head>")
+print("<body>")
+print("<h1>Hello, " + name + "!</h1>")
+print("</body>")
+print("</html>")
