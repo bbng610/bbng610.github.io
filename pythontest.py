b2a178602c77501b7d38059116c5ce20d718427b
Billy Ng <bbng610@gmail.com>
2023-03-20 10:52:48 +0900

    Changes size of Screener

--- pythontest.py
+++ pythontest.py
@@ -1,10 +1,18 @@
 import django
-import requests
+#import requests
 #import os
 #import sys
+import datetime
+
+def get_current_time():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
 
 print(django.__version__)
 
 name ='Billy';
 
 print("Hello ", name);
+
+print (get_current_time())
